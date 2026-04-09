cd11a549baa55ed7a0da9de3a96251ebff2179d3
patricklabyby-stack <patricklabyby@gmail.com>
2026-04-09 22:01:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
 const { Pool } = require("pg");
 const crypto = require("crypto");
-const { registerHelpCommands } = require("./commands/help");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
